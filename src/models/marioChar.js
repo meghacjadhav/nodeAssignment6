a2cd42ca4bf493f24c96377be0b6e0d61d06ff66
nikimegharao@gmail.com <nikimegharao@gmail.com>
2022-09-14 22:25:15 +0530

    Assignment6 commit

---
+++ src/models/marioChar.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+
+//  Your code goes here
+const marioSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  weight: { type: Number, required: true },
+});
+const marioModel = mongoose.model("marioModels", marioSchema);
+module.exports = marioModel;
